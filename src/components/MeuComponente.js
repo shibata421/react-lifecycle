265a11999cf6520ac3d877b586cf7f80fd4a8d78
Fernando Shibata <shibata.421@gmail.com>
2021-01-04 11:15:48 -0300

    métodos de ciclo de vida de componente

--- src/components/MeuComponente.js
+++ src/components/MeuComponente.js
@@ -2,45 +2,40 @@ import React, { Component } from 'react'
 
 class MeuComponente extends Component {
     constructor(props) {
+        // Utilizado para gerar o state inicial e fazer binds
         super(props)
 
         this.state = { numero: 5 }
         this.adicionarNumero = this.adicionarNumero.bind(this)
 
-    
         console.log('construtor....')
     }
 
     static getDerivedStateFromProps(props, state) {
+        // Pouco usado. Atualiza o state a parte do props
         console.log('getDerivedStateFromProps....')
         // console.log(props)
         // console.log(state)
         // return { numero: 6 }
         return null
     }
-
-    componentDidMount() {
-        console.log('componentDidMount...')
-    }
-
+    
     shouldComponentUpdate(nextProps, nextState) {
+        // Possibilita impedir ou autorizar alterações no componente
         console.log('shouldComponentUpdate...')
         console.log('estado atual', this.state)
         console.log('proximo estado', nextState)
+        // Se retornar false, o componente não atualiza
         return true
     }
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate...')
-    }
-
+    
     adicionarNumero() {
         console.log('setState...')
         let numeroAtual = this.state.numero
         numeroAtual++;
         this.setState({ numero: numeroAtual })
     }
-
+    
     render() {
         console.log('render....')
         return(
@@ -54,6 +49,15 @@ class MeuComponente extends Component {
                 </button>
             </div>
         )
+    }
+    
+    componentDidMount() {
+        console.log('componentDidMount...')
+        // Aqui que devem ser feitas as requisições ajax
+    }
+    
+    componentDidUpdate() {
+        console.log('componentDidUpdate...')
     }
 }
 
